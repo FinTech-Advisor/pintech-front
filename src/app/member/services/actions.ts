77a609926713bb0f605739863b37af19d16ea555
rlaehddud475 <6070kjh475@naver.com>
2025-02-07 11:50:39 +0900

    .

--- src/app/member/services/actions.ts
+++ src/app/member/services/actions.ts
@@ -2,6 +2,8 @@
 import { format } from 'date-fns'
 import { redirect } from 'next/navigation'
 
-export const processJoin = async (params, formData: FormData) => {}
+export const processJoin = async (params, formData: FormData) => {
+  v
+}
 export const processLogin = async (params, formData: FormData) => {}
 export const processFindPassword = async (params, formData: FormData) => {}
