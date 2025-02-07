274a1671c1728797410db443dc25bdd48172f4d2
rlaehddud475 <6070kjh475@naver.com>
2025-02-07 18:38:19 +0900

    .

--- src/app/file/download/page.tsx
+++
@@ -1,4 +0,0 @@
-const DownloadPage = () => {
-  return <h1>파일 다운로드</h1>
-}
-export default DownloadPage
--- src/app/file/thumb/page.tsx
+++
@@ -1,4 +0,0 @@
-const ThumbPage = () => {
-  return <h1>썸네일 생성</h1>
-}
-export default ThumbPage
