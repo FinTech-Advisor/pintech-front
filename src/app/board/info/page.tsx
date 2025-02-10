d37dd6954faab583ea23399395d151e282ec150a
LETMEGOZIP <hyuckminkwon23@gmail.com>
2025-02-10 14:45:19 +0900

    20250210 mastser pull

---
+++ src/app/board/info/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+const infoSavePage = () => {
+  return(
+    <>
+      <h1>게시글 작성 및 수정</h1>
+    </>
+  )
+}
+
+export default React.memo(infoSavePage)
---
+++ src/app/board/list/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+const listsavePage = () => {
+  return(
+    <>
+      <h1>게시글 작성 및 수정</h1>
+    </>
+  )
+}
+
+export default React.memo(listsavePage)
