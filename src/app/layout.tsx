8cd278f23362563a3ea1cacf777575626505c5d1
LETMEGOZIP <hyuckminkwon23@gmail.com>
2025-02-13 23:38:42 +0900

    build2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,40 +3,27 @@ import Header from './global/ui/outlines/Header'
 import Footer from './global/ui/outlines/Footer'
 import { CommonProvider } from './global/contexts/CommonContext'
 import { Metadata } from 'next'
-import { getUserInfo } from './member/services/actions'
-import { UserProvider } from './global/contexts/UserContext'
-
-// import { styled } from 'styled-components'
 import 'react-datepicker/dist/react-datepicker.css'
 import './globals.css'
 
-// const MainContent = styled.main`
-//   min-height: 600px;
-// `
-
 export const metadata: Metadata = {
-  title: 'Pintech Project',
-  description: '카드 & 대출 추천 사이트',
+  title: '핀테크 포토폴리오',
+  description: '금융 api 프로젝트',
 }
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const userInfo = await getUserInfo()
-
   return (
     <html lang="ko">
       <body>
         <StyledComponentsRegistry>
-          <UserProvider _userInfo={userInfo}>
-            <CommonProvider>
-              <Header />
-              <main className="main-content">{children}</main>
-              <Footer />
-            </CommonProvider>
-          </UserProvider>
+          <CommonProvider>
+            <Header />
+            <main className="main-content">{children}</main>
+            <Footer />
+          </CommonProvider>
         </StyledComponentsRegistry>
       </body>
     </html>
